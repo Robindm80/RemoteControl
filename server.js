5fe3b802c37c5d6012a59d67bbc2d78d907f5eec
Robindm80 <32535948+Robindm80@users.noreply.github.com>
2017-10-21 21:56:40 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-var http = require('http');
-var fs = require('fs');
+//var http = require('http');
+//var fs = require('fs');
 
-var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
-});
+//var server = http.createServer(function(req, res) {
+    //fs.readFile('./index.html', 'utf-8', function(error, content) {
+        //res.writeHead(200, {"Content-Type": "text/html"});
+       // res.end(content);
+    //});
+//});
 
-var io = require('socket.io').listen(server, { log: false });
-server.listen(process.env.PORT);
+//var io = require('socket.io').listen(server, { log: false });
+//server.listen(process.env.PORT);
 
 //server.listen(3000, '0.0.0.0', function() {
  //   console.log('Listening to port:  ' + 3000);
 //});
+var app = require('express')();
+var http = require('http').Server(app);
 
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
+});
+
+http.listen(3000, function() {
+   console.log('listening on *:3000');
+});
 var allClients = [];
 var line = 0;
 
